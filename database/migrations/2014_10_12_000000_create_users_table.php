9e35ad002c0bb171777a23061962fdd855196741
Mohammed Al Rumhi <malrumhi@omandatapark.com>
2021-10-25 15:43:14 +0400

    add admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('mobile');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('user_type')->default("admin");
+            $table->string('photo')->default("no photo");
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,106 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+
+         <!-- Google Font: Source Sans Pro -->
+         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+         <!-- Font Awesome Icons -->
+         <link rel="stylesheet" href="{{ asset('vendors/plugins/fontawesome-free/css/all.min.css') }}">
+         <!-- IonIcons -->
+         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+         <!-- Theme style -->
+         <link rel="stylesheet" href="{{ asset('vendors/dist/css/adminlte.min.css') }}">
+
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+
+
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+
+        {{-- Start NavBar --}}
+
+
+        @include('layouts.navbar')
+
+        {{-- End NavBar --}}
+
+
+        {{-- Start SideBar --}}
+
+        @include('layouts.sidebar')
+        {{-- End SideBar --}}
+
+
+
+          <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+
+    @yield('content')
+    
+  </div>
+  <!-- /.content-wrapper -->
+
+  
+
+    {{-- Start control --}}
+
+    @include('layouts.control')
+    {{-- End control --}}
+        
+
+
+
+
+</div> 
+
+
+
+
+
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{ asset('vendors/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('vendors/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE -->
+<script src="{{ asset('vendors/dist/js/adminlte.js') }}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{ asset('vendors/plugins/chart.js/Chart.min.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('vendors/dist/js/demo.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('vendors/dist/js/pages/dashboard3.js') }}"></script>
+</body>
+</html>
+
